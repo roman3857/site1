d1a57506ae1892190b0b0f404009cdb572e4a217
roman3857 <romanshakhrai6@gmail.com>
2026-09-03 18:35:33 +0200

    coomit

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const user = {
   surname: `Shakhrai`,
 };
 
-const userTemplete = document.querySelector("#userTemplete");
+const userTemplete = document.querySelector("#countryTemplate");
 const templateFunction = Handlebars.compile(userTemplete.innerHTML);
 const userMarkup = templateFunction(user);
 console.log(userMarkup);
 document.querySelector(".userContainer").innerHTML = userMarkup;
+import countries from "./coutries.json";
